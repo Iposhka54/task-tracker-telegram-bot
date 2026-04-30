ba320d1b33a0e7211d97825e891aabeda0f9077e
Iposhka54 <yaroslavkusb@gmail.com>
2026-04-30 20:28:46 +0300

    refactor: rename variable

--- cmd/main.go
+++ cmd/main.go
@@ -11,14 +11,14 @@ func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	app, err := app.NewApp(logger)
+	application, err := app.NewApp(logger)
 	if err != nil {
 		logger.Fatal("Error while initialize application",
 			zap.Error(err))
 	}
 
 	ctx := context.Background()
-	if err := app.Run(ctx); err != nil {
+	if err := application.Run(ctx); err != nil {
 		logger.Error("application error",
 			zap.Error(err),
 		)
